d0b401fad75668c787745ee286fc0d1502cc73f1
Luís Wolf <luis.wolf@getnet.com.br>
2018-11-24 19:07:03 -0200

    XCode project created

---
+++ NextMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextMovie
+//
+//  Created by Luís Wolf on 24/11/2018.
+//  Copyright © 2018 Luís Wolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
